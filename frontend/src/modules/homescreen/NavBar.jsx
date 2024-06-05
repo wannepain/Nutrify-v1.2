d686cf524b73e6e5d07b0a44e8151ada6cd92057
Marek Wannepain <marekwannepain@gmail.com>
2024-06-05 07:30:10 +0200

    Finished the navbar component

--- frontend/src/modules/homescreen/NavBar.jsx
+++ frontend/src/modules/homescreen/NavBar.jsx
@@ -1,33 +1,49 @@
+import { useEffect, useState } from "react";
 import classes from "./navbar.module.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function NavBar(props) {
+    const location = useLocation()
+    const [currentPage, setCurrentPage] = useState(""); 
+    
+    useEffect(() => {
+      if (location.pathname === "/home") {
+        setCurrentPage("home")
+      } else {
+        setCurrentPage("add")
+      }
+        
+    }, [location])
+    
+    
     return (
         <div>
             <div className={`${classes.navbar} ${classes.hidden}`}>
                 <ul className={classes.container}>
-                    <li className={classes.imgContainer}><img src="./../../../public/nutrify.svg" alt="nutrify" className={classes.nutrifyLogo}/></li>
+                    <li className={classes.imgContainer}>
+                        <img src="/public/nutrify.svg" alt="nutrify" className={classes.nutrifyLogo} />
+                    </li>
                     <li className={classes.linkContainer}>
-                        <Link to="/home">Home</Link>
-                        <Link to="add">Add Recipe</Link>
+                        <Link to="/home" className={currentPage === "home" && classes.currentLink}>Home</Link>
+                        <Link to="add" className={currentPage === "add" && classes.currentLink}>Add Recipe</Link>
                     </li>
                 </ul>
             </div>
-             <footer className={classes.phoneNavBar}>
-                 <ul className={classes.phoneContainer}>
-                     <li>
-                         <Link to="/home" className={classes.iconBtn}>
-                             <img src="/home_icon.svg" alt="Home" />
-                         </Link>
-                     </li>
-                     <li>
-                         <Link to="add" className={classes.iconBtn}>
-                             <img src="/add_recipe_icon.svg" alt="Add recipe" />
-                         </Link>
-                     </li>
-                 </ul>
-             </footer>
-         </div>
+            <footer className={classes.phoneNavBar}>
+                <ul className={classes.phoneContainer}>
+                    <li>
+                        <Link to="/home" className={classes.iconBtn}>
+                            <img src="/home_icon.svg" alt="Home" className={currentPage === "home" && classes.currentIcon}/>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="add" className={classes.iconBtn} >
+                            <img src="/add_recipe_icon.svg" alt="Add recipe" className={currentPage === "add" && classes.currentIcon}/>
+                        </Link>
+                    </li>
+                </ul>
+            </footer>
+        </div>
     );
 }
 
