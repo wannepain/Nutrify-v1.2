3df84b1325d5083188f8b3f093cebd3702353b5a
Marek Wannepain <marekwannepain@gmail.com>
2024-06-14 06:48:32 +0200

    worked on the welcome screen, going to go for this type of design https://www.ozone.pro/

--- frontend/src/pages/authentication/Welcome.jsx
+++ frontend/src/pages/authentication/Welcome.jsx
@@ -13,12 +13,23 @@ function Welcome(props) {
 
     return(
         <div className={classes.body}>
-            <div>
-            <h1>Welcome to nutrify</h1>
-            <div className={classes.containerInline}>
-                <Link className={classes.btn} to="signup">Sign Up</Link>
-                <Link className={classes.btn} to="login">Log in</Link>
-            </div>
+            <div className={classes.main}>
+                <h1 className={classes.mainHeading}>Welcome to nutrify</h1>
+                <div className={classes.sideBySideDiv}>
+
+                    <div className={classes.infoContainer}>
+                        <h1 className={classes.heading}>Lorem ipsum dolor sit amet</h1>
+                        <p className={classes.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Proin sagittis nisl rhoncus mattis rhoncus urna. Sed blandit libero volutpat sed cras ornare arcu. Vitae proin sagittis nisl rhoncus mattis rhoncus urna. Duis tristique sollicitudin nibh sit amet commodo nulla facilisi. Ipsum consequat nisl vel pretium lectus. Tristique senectus et netus et malesuada fames ac. Faucibus purus in massa tempor nec feugiat nisl. Dui faucibus in ornare quam viverra orci sagittis. Leo a diam sollicitudin tempor id eu nisl. Etiam erat velit scelerisque in dictum non consectetur. Sagittis nisl rhoncus mattis rhoncus. Tortor at risus viverra adipiscing at in tellus integer. Neque gravida in fermentum et sollicitudin. Massa id neque aliquam vestibulum morbi blandit cursus. Lobortis elementum nibh tellus molestie nunc non blandit. Nulla pharetra diam sit amet nisl suscipit. Massa sed elementum tempus egestas sed sed risus.</p>
+                    </div>
+                    <div className={classes.sign}>
+                        <h1 className={classes.heading}>Lorem ipsum dolor sit amet</h1>
+                        <div className={classes.containerInline}>
+                            <Link className={classes.btn} to="signup">Sign Up</Link>
+                            <Link className={classes.btn} to="login">Log in</Link>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
